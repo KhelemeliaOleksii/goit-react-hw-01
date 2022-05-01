52726adc01da61f54393e886378cf2af432da3cb
KhelemeliaOleksii <xvdm@ukr.net>
2022-05-02 00:34:40 +0300

    add style to transaction history

--- src/components/transactionHistory/transactionHistory.jsx
+++ src/components/transactionHistory/transactionHistory.jsx
@@ -1,9 +1,12 @@
 import { TransactionHistoryTableHeader } from './transactionHistoryTableHeader'
 import { TransactionHistoryTableBody} from './transactionHistoryTableBody'
 
+import styles from './css/table.module.css'
 export function TransactionHistory({items}) {
+    const tableStyles = [styles.table];
+
     return (
-        <table className="transaction-history">
+        <table className={tableStyles}>
             <TransactionHistoryTableHeader />
             <TransactionHistoryTableBody data = {items}/>
         </table >
